926f4544fd8b5d74e0f41c176d62582a3989b9b9
rasputin <thomas.1.andersen@gmail.com>
2023-04-07 13:48:01 +0200

    Copy headers to logheaders

--- UserService/QueueTriggerUserService.cs
+++ UserService/QueueTriggerUserService.cs
@@ -23,7 +23,13 @@ namespace UserService
             {
                 PropertyNamingPolicy = JsonNamingPolicy.CamelCase
             });
+            var logMessage = new Message();
             try {
+                List<MessageHeader> headers = new List<MessageHeader>();
+                headers.Add(new MessageHeader() { Name = "id-header", Fields = new Dictionary<string, string>() { { "GUID", message.Headers.FirstOrDefault(x => x.Name.Equals("id-header")).Fields["GUID"] } } });
+                headers.Add(new MessageHeader() { Name = "current-queue-header", Fields = new Dictionary<string, string>() { { "Name", message.Headers.FirstOrDefault(x => x.Name.Equals("current-queue-header")).Fields["Name"] }, { "Timestamp", message.Headers.FirstOrDefault(x => x.Name.Equals("current-queue-header")).Fields["Timestamp"] } } });
+                logMessage.Headers = headers.ToArray();
+
                 var cmd = JsonSerializer.Deserialize<CmdUser>(message.Body, new JsonSerializerOptions
                 {
                     PropertyNamingPolicy = JsonNamingPolicy.CamelCase
@@ -39,12 +45,12 @@ namespace UserService
                     log.LogError($"Command {cmd.Command} not supported");
                 }
                 stopwatch.Stop();
-                await MessageHelper.SendLog(message, receivedMessageTime, stopwatch.ElapsedMilliseconds);
+                await MessageHelper.SendLog(logMessage, receivedMessageTime, stopwatch.ElapsedMilliseconds);
             } catch(Exception ex) {
-                var current = message.Headers.FirstOrDefault(x => x.Name.Equals("current-queue-header"));
+                var current = logMessage.Headers.FirstOrDefault(x => x.Name.Equals("current-queue-header"));
                 current.Fields["Name"] = current.Fields["Name"] + $"-Error (User): {ex.Message}";
                 stopwatch.Stop();
-                await MessageHelper.SendLog(message, receivedMessageTime, stopwatch.ElapsedMilliseconds);
+                await MessageHelper.SendLog(logMessage, receivedMessageTime, stopwatch.ElapsedMilliseconds);
             }
 
         }
